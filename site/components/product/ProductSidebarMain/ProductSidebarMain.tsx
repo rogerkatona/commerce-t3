8dab963e38706fd198ffdfeeb6fce8290624f608
Roger Katona <roger.katona@gmail.com>
2022-12-02 15:12:09 -0500

    Continuing to port over parts of the original site. 12.2 3:12

--- site/components/product/ProductSidebarMain/ProductSidebarMain.tsx
+++ site/components/product/ProductSidebarMain/ProductSidebarMain.tsx
@@ -51,11 +51,6 @@ const ProductSidebarMain: FC<ProductSidebarProps> = ({ product, className }) => 
 
   return (
     <div className={className}>
-      <ProductOptions
-        options={product.options}
-        selectedOptions={selectedOptions}
-        setSelectedOptions={setSelectedOptions}
-      />
       <div className='font-bebasNeue text-5xl text-gray-300 pt-6 lg:pl-0 pl-6'>
         {product.name}
       </div>
@@ -66,9 +61,17 @@ const ProductSidebarMain: FC<ProductSidebarProps> = ({ product, className }) => 
         className=" w-full max-w-xl lg:px-0  px-6"
         html={product.descriptionHtml || product.description}
       />
+      <ProductOptions
+        options={product.options}
+        selectedOptions={selectedOptions}
+        setSelectedOptions={setSelectedOptions}
+      />
       <div className="flex flex-row justify-between items-center lg:pl-0  pl-6">
-        <Rating value={4} />
-        <div className="text-accent-6 pr-1 font-medium text-sm">36 reviews</div>
+        <Rating value={5} />
+        <div className="text-accent-6 pr-1 font-medium text-sm">36 Reviews</div>
+      </div>
+      <div className="flex flex-row justify-between items-center lg:pl-0  pl-6">
+
       </div>
       <div className={`${+product.id === 165  ? 'hidden' : 'block'} `}>
         {process.env.COMMERCE_CART_ENABLED && (
@@ -86,17 +89,6 @@ const ProductSidebarMain: FC<ProductSidebarProps> = ({ product, className }) => 
           </Button>
         )}
       </div>
-{/*      <div className="mt-6">
-        <Collapse title="Care">
-          This is a limited edition production run. Printing starts when the
-          drop ends.
-        </Collapse>
-        <Collapse title="Details">
-          This is a limited edition production run. Printing starts when the
-          drop ends. Reminder: Bad Boys For Life. Shipping may take 10+ days due
-          to COVID-19.
-        </Collapse>
-      </div>*/}
     </div>
   )
 }
